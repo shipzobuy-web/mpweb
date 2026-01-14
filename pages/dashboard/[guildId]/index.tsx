aaf35390b135a61dbe857d0049cc718449985af2
Shipzobuy Global <shipzobuy@gmail.com>
2026-01-14 20:27:24 +0100

    Update index.tsx

--- pages/dashboard/[guildId]/index.tsx
+++ pages/dashboard/[guildId]/index.tsx
@@ -2,16 +2,30 @@ import { Box, Button, Flex, Icon, Text } from "@chakra-ui/react"
 import { getSession } from "next-auth/client"
 import BaseLayout from "../../../components/BaseLayout"
 import { FetchBotGuilds, FetchGuildDoc } from "../../../lib/DbUtils"
-import Config from "../../../config.json"
 import { FaHammer, FaPlus, FaUser } from "react-icons/fa"
 import { BsChatFill } from "react-icons/bs"
 
 const StatBox = ({ icon, color, data, label }) => {
     return (
-        <Flex minW="90px" w="max-content" h="auto" bg={color} p={5} rounded="md" justifyContent="space-between" gap={5} alignItems="center" className="animate">
+        <Flex
+            minW="90px"
+            w="max-content"
+            h="auto"
+            bg={color}
+            p={5}
+            rounded="md"
+            justifyContent="space-between"
+            gap={5}
+            alignItems="center"
+            className="animate"
+        >
             <Flex direction="column" justifyContent="center" alignItems="center" className="animate">
-                <Text className="animate" fontSize="2.5vw" textAlign="center">{data}</Text>
-                <Text className="animate" fontSize="1vw" textAlign="center" wordBreak={"break-all"}>{label}</Text>
+                <Text className="animate" fontSize="2.5vw" textAlign="center">
+                    {data}
+                </Text>
+                <Text className="animate" fontSize="1vw" textAlign="center" wordBreak="break-all">
+                    {label}
+                </Text>
             </Flex>
             <Icon as={icon} fontSize="4vw" className="animate" />
         </Flex>
@@ -19,7 +33,6 @@ const StatBox = ({ icon, color, data, label }) => {
 }
 
 export default function GuildDashboard({ session, guild }) {
-
     return (
         <>
             {!guild && (
@@ -32,7 +45,7 @@ export default function GuildDashboard({ session, guild }) {
                             colorScheme="brand.blue"
                             onClick={() =>
                                 window.open(
-                                    `https://discord.com/api/oauth2/authorize?client_id=${Config.clientId}&permissions=8&scope=bot%20applications.commands`
+                                    `https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&permissions=8&scope=bot%20applications.commands`
                                 )
                             }
                             h="60px"
@@ -48,10 +61,15 @@ export default function GuildDashboard({ session, guild }) {
             {guild && (
                 <BaseLayout pageTitle={guild.name} navGuild={guild}>
                     <Flex wrap="wrap" justifyContent="center" alignItems="center" direction="row" gap={5}>
-                        <StatBox label={"Server Members"} color={"#1e65f5"} data={guild.member_count} icon={FaUser} />
-                        <StatBox label={"Banned Members"} color={"#f64f52"} data={guild.ban_count} icon={FaHammer} />
-                        <StatBox label={"Invites Created"} color={"#37ef48"} data={guild.invite_count} icon={FaPlus} />
-                        <StatBox label={"Server Channels"} color={"#e646f3"} data={guild.channels.length} icon={BsChatFill} />
+                        <StatBox label="Server Members" color="#1e65f5" data={guild.member_count} icon={FaUser} />
+                        <StatBox label="Banned Members" color="#f64f52" data={guild.ban_count} icon={FaHammer} />
+                        <StatBox label="Invites Created" color="#37ef48" data={guild.invite_count} icon={FaPlus} />
+                        <StatBox
+                            label="Server Channels"
+                            color="#e646f3"
+                            data={guild.channels.length}
+                            icon={BsChatFill}
+                        />
                     </Flex>
                 </BaseLayout>
             )}
@@ -62,11 +80,19 @@ export default function GuildDashboard({ session, guild }) {
 export const getServerSideProps = async (ctx) => {
     const session = await getSession(ctx)
     const { guildId } = ctx.query
-    let userGuild
 
-    if (session) userGuild = session.guilds.find(g => g.id == guildId)
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/login",
+                permanent: false
+            }
+        }
+    }
 
-    if (!session || !userGuild) {
+    const userGuild = session.guilds.find(g => g.id === guildId)
+
+    if (!userGuild) {
         return {
             redirect: {
                 destination: "/login",
@@ -76,7 +102,7 @@ export const getServerSideProps = async (ctx) => {
     }
 
     const botGuilds = await FetchBotGuilds()
-    let guild = botGuilds.find(g => g._id == guildId) || null
+    let guild = botGuilds.find(g => g._id === guildId) || null
 
     if (guild) {
         guild = {
