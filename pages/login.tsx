49522f93b10b99b508d7746e02528fb3c87cd4a7
Shipzobuy Global <shipzobuy@gmail.com>
2026-01-14 18:33:48 +0100

    fixed vercel deployment

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,14 +2,13 @@ import { AbsoluteCenter, Button, Flex, Heading } from "@chakra-ui/react";
 import { signIn } from "next-auth/react";
 import BaseLayout from "../components/BaseLayout";
 import { FaDiscord } from "react-icons/fa";
-import { useState } from "react";
 
 export default function SignIn() {
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = React.useState(false);
 
   const handleLogin = async () => {
     setLoading(true);
-    // Use NextAuth's built-in redirect handling
+    // Uses NextAuth v4 redirect handling
     await signIn("discord", {
       callbackUrl: "/dashboard", // redirect after login
     });
