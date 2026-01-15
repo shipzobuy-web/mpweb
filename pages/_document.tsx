3bf3a9008ce5a751ca9d0a058e4717882a547b4f
Shipzobuy Global <shipzobuy@gmail.com>
2026-01-15 14:41:15 +0100

    Update font to Poppins and import theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,21 @@
-import { ColorModeScript, theme } from "@chakra-ui/react"
+import { ColorModeScript } from "@chakra-ui/react"
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+import theme from "../styles/theme"
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* Preconnect for Google Fonts */}
+          {/* Poppins font */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
         </Head>
         <body>
-          {/* Chakra Color Mode */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
