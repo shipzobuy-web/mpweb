3c9dd57d94ad42b34b4a1a0ac3a4ddbc2bfd3255
Shipzobuy Global <shipzobuy@gmail.com>
2026-01-15 10:21:22 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,26 @@
-import { ColorModeScript, theme } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript, theme } from "@chakra-ui/react"
+import NextDocument, { Html, Head, Main, NextScript } from "next/document"
 
 export default class Document extends NextDocument {
-    render() {
-        return (
-            <Html lang='en'>
-                <Head />
-                <body>
-                    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Preconnect for Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          {/* Chakra Color Mode */}
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
