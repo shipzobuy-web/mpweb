5d3f7a24cc1866184ed01b2734e31c7fabc84c01
Shipzobuy Global <shipzobuy@gmail.com>
2026-01-15 10:16:25 +0100

    Update index.tsx

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,73 +1,84 @@
-import { Avatar, Flex, ScaleFade, Text, Tooltip } from "@chakra-ui/react";
-import BaseLayout from "../../components/BaseLayout";
-import { getSession } from "next-auth/client";
-import { FetchBotGuilds } from "../../lib/DbUtils";
-import { GetGuildIcon } from "../../lib/Utils";
-import { useRouter } from "next/router";
+import { Avatar, Flex, ScaleFade, Text, Tooltip, Badge } from "@chakra-ui/react"
+import BaseLayout from "../../components/BaseLayout"
+import { getSession } from "next-auth/client"
+import { FetchBotGuilds } from "../../lib/DbUtils"
+import { useRouter } from "next/router"
 
-export default function DashboardIndex({ session, botGuilds }) {
-    const router = useRouter();
+export default function DashboardIndex({ session, botGuildIds }) {
+    const router = useRouter()
 
     return (
         <BaseLayout pageTitle="Dashboard">
             <Flex justifyContent="center">
                 <Avatar
                     w="100px"
-                    h="auto"
                     src={session.user.image}
-                    className="animate"
                     name={session.user.name}
                 />
             </Flex>
 
-            <Text
-                mt="20.5px"
-                className="animate"
-                textAlign="center"
-                fontSize={{ base: "5vw", sm: "25px" }}
-            >
-                {session.user.name}, select a server!
+            <Text mt="20px" textAlign="center" fontSize="25px">
+                {session.user.name}, select a server
             </Text>
 
-            <Flex wrap="wrap" mt="3vw" p={10} justifyContent="center">
-                {botGuilds.map((guild, index) => (
-                    <ScaleFade key={`guild-${guild._id}`} initialScale={0.4} in={true} delay={1 + index / 10}>
-                        <Tooltip label={guild.name} bg="black" fontWeight="semibold" hasArrow>
-                            <Avatar
-                                cursor="pointer"
-                                className="animate"
-                                src={GetGuildIcon(guild)}
-                                name={guild.name}
-                                boxSize="80px"
-                                m={3}
-                                _hover={{ transform: "scale(1.1)" }}
-                                onClick={() => router.push(`/dashboard/${guild._id}/toggle`)}
-                            />
-                        </Tooltip>
-                    </ScaleFade>
-                ))}
+            <Flex wrap="wrap" mt={10} justifyContent="center">
+                {session.guilds.map((guild, index) => {
+                    const hasBot = botGuildIds.includes(guild.id)
+                    const icon = guild.icon
+                        ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`
+                        : undefined
+
+                    return (
+                        <ScaleFade key={guild.id} initialScale={0.4} in delay={0.1 * index}>
+                            <Tooltip label={guild.name} hasArrow>
+                                <Flex
+                                    direction="column"
+                                    align="center"
+                                    m={3}
+                                    cursor="pointer"
+                                    onClick={() =>
+                                        hasBot
+                                            ? router.push(`/dashboard/${guild.id}`)
+                                            : window.open(
+                                                  `https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&permissions=8&scope=bot%20applications.commands`
+                                              )
+                                    }
+                                >
+                                    <Avatar
+                                        boxSize="80px"
+                                        src={icon}
+                                        name={guild.name}
+                                    />
+                                    {!hasBot && (
+                                        <Badge mt={2} colorScheme="red">
+                                            Invite Bot
+                                        </Badge>
+                                    )}
+                                </Flex>
+                            </Tooltip>
+                        </ScaleFade>
+                    )
+                })}
             </Flex>
         </BaseLayout>
-    );
+    )
 }
 
 export const getServerSideProps = async (ctx) => {
-    const session = await getSession(ctx);
+    const session = await getSession(ctx)
     if (!session) {
         return {
             redirect: { destination: "/login", permanent: false }
-        };
+        }
     }
 
-    // Only include guilds where the bot is actually present
-    const botGuilds = await FetchBotGuilds();
-    const userGuilds = session.guilds.map(g => g.id);
-    const filteredGuilds = botGuilds.filter(g => userGuilds.includes(g._id));
+    const botGuilds = await FetchBotGuilds()
+    const botGuildIds = botGuilds.map(g => g._id)
 
     return {
         props: {
             session,
-            botGuilds: filteredGuilds
+            botGuildIds
         }
-    };
-};
+    }
+}
